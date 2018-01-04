6b35df46e89659a220fc124c5e8c7889f0c26f91
Shrishtika <shrishtika2009@gmail.com>
2018-01-04 17:37:35 +0530

    Add files via upload

---
+++ Decode.java
@@ -0,0 +1,35 @@
+package com.challenge;
+
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Decode {
+
+	private Decode() {
+
+	}
+
+	public static String decode(String source) {
+		StringBuilder dest = new StringBuilder();
+		Pattern pattern = Pattern.compile("[0-9]+|[a-zA-Z]");
+		Matcher matcher = pattern.matcher(source);
+		while (matcher.find()) {
+			int number = Integer.parseInt(matcher.group());
+			matcher.find();
+			while (number-- != 0) {
+				dest.append(matcher.group());
+			}
+		}
+		return dest.toString();
+	}
+
+	public static void main(String[] args) {
+		Logger l = Logger.getLogger("Decoded");
+		String str = "3a2b1c";
+		String decoded = decode(str);
+		l.info(decoded);
+
+	}
+
+}
